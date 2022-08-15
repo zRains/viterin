ff35d7554b05d01c11187544daab1b9bc4937721
zRain <2296342883@qq.com>
2022-08-15 20:36:57 +0800

    feat: detach package

--- docs/.vitepress/theme/components/page/VRPageFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="VRPageFooter"></div>
-</template>
-
-<script setup lang="ts"></script>
-
-<style lang="scss"></style>
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,28 +1,4 @@
-import { Theme } from 'vitepress'
+import Theme from 'vitepress-theme-viterin'
 
-// Global Components
-import { Icon } from '@iconify/vue'
-import CenterImg from './components/global/CenterImg.vue'
-import BookMark from './components/global/BookMark.vue'
-import VRFriends from './components/VRFriends.vue'
+export default Theme
 
-// Layout
-import Layout from './Layout.vue'
-import NotFound from './NotFound.vue'
-
-// Styles
-import './styles/basic.scss'
-import './styles/colors.scss'
-
-const theme: Theme = {
-  Layout,
-  NotFound,
-  enhanceApp({ app }) {
-    app.component('Icon', Icon)
-    app.component('CenterImg', CenterImg)
-    app.component('BookMark', BookMark)
-    app.component('VRFriends', VRFriends)
-  }
-}
-
-export default theme
